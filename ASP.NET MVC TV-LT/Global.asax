d60d61f3efbd9b3a600724771ccab9e1d17a8700
= <=>
2020-09-11 15:30:44 +0700

    Up project len github

---
+++ ASP.NET MVC TV-LT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_MVC_TV_LT.MvcApplication" Language="C#" %>
---
+++ ASP.NET MVC TV-LT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC_TV_LT.Startup))]
+namespace ASP.NET_MVC_TV_LT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
